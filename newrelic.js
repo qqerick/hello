6246c961836627624c8ce62bd1586422d7ad5412
RaghdaSallam <raghda99@live.com>
2025-09-13 18:59:36 +0300

    Adding new relic and doing some updates to the code

--- newrelic.js
+++ newrelic.js
@@ -1,9 +1,9 @@
-'use strict'
+'use strict';
 
 exports.config = {
   app_name: ['Hello World Node App'],
-  license_key: 'eu01xxcf8a740dddf1ade4244bcb83cdFFFFNRAL',
+  license_key: process.env.NEW_RELIC_LICENSE_KEY,
   logging: {
     level: 'info',
   },
-}
+};
